a27187d90bd9ae138c5bc7a3b3ec825e6c3de880
Armando <avazquez102@alumnos.uaq.mx>
2023-06-13 22:51:45 -0600

    commit inicial

---
+++ src/main/java/Script/Alerts.java
@@ -0,0 +1,19 @@
+package Script;
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import poms.Googlepages;
+
+public class Alerts {
+	
+
+
+		
+}
